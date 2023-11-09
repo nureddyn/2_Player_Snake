e925fecfc9c40cdbeebd200cf0299859e7540a46
nureddyn <nureddynballout@gmail.com>
2023-11-08 21:00:54 -0300

    Board class improved

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 class Board {
-    constructor() {
+    constructor(blockSize, rows, columns) {
         this.domElement = document.querySelector('.board');
         this.context = this.domElement.getContext('2d');
-        this.blockSize = 25;
-        this.rows = 20;
-        this.columns = 20;
-        this.height = this.rows * this.blockSize;
-        this.width = this.columns * this.blockSize; 
+        this.blockSize = blockSize;
+        this.rows = rows;
+        this.columns = columns;
+        this.domElement.height = this.rows * this.blockSize;
+        this.domElement.width = this.columns * this.blockSize; 
     }
 
-    make() {
-        this.context.fillStyle = 'black';
-        this.context.fillRect(0, 0, board.height, board.width);
+    make(color='black') {
+        this.context.fillStyle = color;
+        this.context.fillRect(0, 0, this.domElement.height, this.domElement.width);
     }
 }
 
 
-let board = new Board();
+let board = new Board(25, 20, 20);
 
 document.addEventListener('DOMContentLoaded', () => {
     board.make();
 });
+
